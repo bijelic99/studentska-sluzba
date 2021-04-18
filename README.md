cf3083a193e06a37d77e9123698c6415da69dcf0
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2021-04-18 15:56:49 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# studentska-sluzba
---
+++ src/test/java/com/ftn/studentskasluzba/StudentskaSluzbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.studentskasluzba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentskaSluzbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
