cb495b2a1dee8636a2cfd5e6b30e28194919adb9
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2021-05-28 21:38:42 +0200

    Added some course endpoints

--- src/main/java/com/ftn/studentskasluzba/controller/CourseController.java
+++ src/main/java/com/ftn/studentskasluzba/controller/CourseController.java
@@ -1,6 +1,6 @@
 package com.ftn.studentskasluzba.controller;
 
-import com.ftn.studentskasluzba.dto.CourseDTO;
+import com.ftn.studentskasluzba.dto.*;
 import com.ftn.studentskasluzba.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/courses")
@@ -23,8 +25,59 @@ public class CourseController {
         try {
             var course = courseRepository.findById(id).map(CourseDTO::new).orElseThrow();
             return ResponseEntity.ok(course);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
         }
-        catch (NoSuchElementException e) {
+    }
+
+    @GetMapping("/{id}/enrolments")
+    public ResponseEntity<Collection<CourseEnrolmentDTO>> getCourseEnrolments(@PathVariable("id") Long id) {
+        try {
+            var enrolments = courseRepository.findById(id).stream()
+                    .flatMap(x -> x.getCourseEnrolments().stream())
+                    .map(CourseEnrolmentDTO::new)
+                    .collect(Collectors.toSet());
+            return ResponseEntity.ok(enrolments);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{id}/teachers")
+    public ResponseEntity<Collection<TeacherDTO>> getCourseTeachers(@PathVariable("id") Long id) {
+        try {
+            var teachers = courseRepository.findById(id).stream()
+                    .flatMap(x -> x.getTeachers().stream())
+                    .map(TeacherDTO::new)
+                    .collect(Collectors.toSet());
+            return ResponseEntity.ok(teachers);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{id}/exams")
+    public ResponseEntity<Collection<ExamDTO>> getCourseExams(@PathVariable("id") Long id) {
+        try {
+            var exams = courseRepository.findById(id).stream()
+                    .flatMap(x -> x.getExams().stream())
+                    .map(ExamDTO::new)
+                    .collect(Collectors.toSet());
+            return ResponseEntity.ok(exams);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{id}/additional-points")
+    public ResponseEntity<Collection<CourseAdditionalPointsDTO>> getCourseAdditionalPoints(@PathVariable("id") Long id) {
+        try {
+            var cap = courseRepository.findById(id).stream()
+                    .flatMap(x -> x.getCourseAdditionalPoints().stream())
+                    .map(CourseAdditionalPointsDTO::new)
+                    .collect(Collectors.toSet());
+            return ResponseEntity.ok(cap);
+        } catch (NoSuchElementException e) {
             return ResponseEntity.notFound().build();
         }
     }
--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -27,3 +27,18 @@ INSERT INTO TEACHER(id, course_id, employee_id, teaching_role_id) VALUES (8, 5, 
 INSERT INTO TEACHER(id, course_id, employee_id, teaching_role_id) VALUES (9, 5, 3, 1);
 INSERT INTO TEACHER(id, course_id, employee_id, teaching_role_id) VALUES (10, 5, 1, 3);
 
+INSERT INTO EXAM(id, exam_date_time, exam_enrolment_fee, course_id) VALUES(1, '2021-06-29 00:00:00', 200.00, 1);
+INSERT INTO EXAM(id, exam_date_time, exam_enrolment_fee, course_id) VALUES(2, '2021-06-29 00:00:00', 200.00, 2);
+INSERT INTO EXAM(id, exam_date_time, exam_enrolment_fee, course_id) VALUES(3, '2021-06-29 00:00:00', 200.00, 3);
+INSERT INTO EXAM(id, exam_date_time, exam_enrolment_fee, course_id) VALUES(4, '2021-06-29 00:00:00', 200.00, 4);
+INSERT INTO EXAM(id, exam_date_time, exam_enrolment_fee, course_id) VALUES(5, '2021-06-29 00:00:00', 200.00, 5);
+
+INSERT INTO course_additional_points_type(id, name, pass_required) VALUES(1, 'Project', true);
+INSERT INTO course_additional_points_type(id, name, pass_required) VALUES(2, 'Colloquium', true);
+INSERT INTO course_additional_points_type(id, name, pass_required) VALUES(3, 'Homework', false);
+
+INSERT INTO course_additional_points(id, name, course_id, course_additional_points_type_id) VALUES(1, "First Colloquium", 1, 2);
+INSERT INTO course_additional_points(id, name, course_id, course_additional_points_type_id) VALUES(2, "Second Colloquium", 1, 2);
+INSERT INTO course_additional_points(id, name, course_id, course_additional_points_type_id) VALUES(3, "Math homework", 3, 3);
+INSERT INTO course_additional_points(id, name, course_id, course_additional_points_type_id) VALUES(4, "Math homework", 4, 3);
+INSERT INTO course_additional_points(id, name, course_id, course_additional_points_type_id) VALUES(5, "Final project", 1, 1);
