1dc27ddbeb2459801c9e88593d2ee1b45d9326e3
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2021-07-03 15:25:38 +0200

    Extended course end dates

--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -10,11 +10,11 @@ INSERT INTO SUBJECT(id, ects, title) VALUES (1, 6, 'E learning');
 INSERT INTO SUBJECT(id, ects, title) VALUES (2, 8, 'Mathematics I');
 INSERT INTO SUBJECT(id, ects, title) VALUES (3, 8, 'Mathematics II');
 
-INSERT INTO COURSE(id, start_date, end_date, subject_id) VALUES(1, '2021-01-01 00:00:00', '2021-07-01 00:00:00', 1);
+INSERT INTO COURSE(id, start_date, end_date, subject_id) VALUES(1, '2021-01-01 00:00:00', '2021-10-01 00:00:00', 1);
 INSERT INTO COURSE(id, start_date, end_date, subject_id) VALUES(2, '2020-07-01 00:00:00', '2021-01-01 00:00:00', 1);
 INSERT INTO COURSE(id, start_date, end_date, subject_id) VALUES(3, '2020-07-01 00:00:00', '2021-01-01 00:00:00', 2);
-INSERT INTO COURSE(id, start_date, end_date, subject_id) VALUES(4, '2021-01-01 00:00:00', '2021-07-01 00:00:00', 3);
-INSERT INTO COURSE(id, start_date, end_date, subject_id) VALUES(5, '2021-01-01 00:00:00', '2021-07-01 00:00:00', 3);
+INSERT INTO COURSE(id, start_date, end_date, subject_id) VALUES(4, '2021-01-01 00:00:00', '2021-10-01 00:00:00', 3);
+INSERT INTO COURSE(id, start_date, end_date, subject_id) VALUES(5, '2021-01-01 00:00:00', '2021-10-01 00:00:00', 3);
 
 INSERT INTO TEACHER(id, course_id, employee_id, teaching_role_id) VALUES (1, 1, 1, 1);
 INSERT INTO TEACHER(id, course_id, employee_id, teaching_role_id) VALUES (2, 1, 2, 2);
