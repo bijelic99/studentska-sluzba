5fc09789e0e7b8ebd4c83651c9ee87e9d99322b8
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2021-07-11 13:39:04 +0200

    Login finished on FE

--- src/main/java/com/ftn/studentskasluzba/configurations/SecurityConfigurer.java
+++ src/main/java/com/ftn/studentskasluzba/configurations/SecurityConfigurer.java
@@ -2,22 +2,15 @@ package com.ftn.studentskasluzba.configurations;
 
 import com.ftn.studentskasluzba.filters.JwtRequestFilter;
 import com.ftn.studentskasluzba.service.ApplicationUserDetailsService;
-import com.ftn.studentskasluzba.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -47,10 +40,13 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/authenticate/*", "/register").permitAll()
+        http
+                .csrf().disable()
+                .authorizeRequests().antMatchers("/authenticate/*", "/register").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        http.cors();
     }
 }
