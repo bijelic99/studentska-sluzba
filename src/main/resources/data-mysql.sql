aeac8f38c249e9d7b0850e9cfd476b0f51a14741
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2022-01-23 19:17:52 +0100

    Added additional data to data-mysql, updated dates

--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -3,6 +3,8 @@ INSERT INTO ADMIN(id, email, first_name, last_name, password, username, deleted)
 INSERT INTO EMPLOYEE(id, email, first_name, last_name, password, username, deleted) VALUES (1, 'e1@mail.com', 'Employe', 'One', '$2y$12$Ejrxz18IUDZZ1IAWQhimC.mBoTkLl.OkdfcI8TWDoj9Dy2UDes2A2', 'eOne', false); --password1
 INSERT INTO EMPLOYEE(id, email, first_name, last_name, password, username, deleted) VALUES (2, 'e2@mail.com', 'Employe', 'Two', '$2y$12$Pjgz3E4P43eoUGanEYYFOuY06d/3vUtqqrZ28Jxn0eNel7GsTe6Z2', 'eTwo', false); --password2
 INSERT INTO EMPLOYEE(id, email, first_name, last_name, password, username, deleted) VALUES (3, 'e3@mail.com', 'Employe', 'Three', '$2y$12$dZ9azHLaXE3oLEkoLWmuFepccWfedqYJt9P3YQlBa6Y/sg5ImtsvC', 'eThree', false); --password3
+INSERT INTO EMPLOYEE(id, email, first_name, last_name, password, username, deleted) VALUES (4, 'e4@mail.com', 'Employe', 'Four', '$2y$12$Pjgz3E4P43eoUGanEYYFOuY06d/3vUtqqrZ28Jxn0eNel7GsTe6Z2', 'eFour', false); --password2
+INSERT INTO EMPLOYEE(id, email, first_name, last_name, password, username, deleted) VALUES (5, 'e5@mail.com', 'Employe', 'Five', '$2y$12$dZ9azHLaXE3oLEkoLWmuFepccWfedqYJt9P3YQlBa6Y/sg5ImtsvC', 'eFive', false); --password3
 
 INSERT INTO TEACHING_ROLE(id, name, deleted) VALUES (1, 'Professor', false);
 INSERT INTO TEACHING_ROLE(id, name, deleted) VALUES (2, 'Assistant', false);
@@ -13,12 +15,17 @@ INSERT INTO SUBJECT(id, ects, title, deleted) VALUES (2, 8, 'Mathematics I', fal
 INSERT INTO SUBJECT(id, ects, title, deleted) VALUES (3, 8, 'Mathematics II', false);
 INSERT INTO SUBJECT(id, ects, title, deleted) VALUES (4, 8, 'Web', false);
 INSERT INTO SUBJECT(id, ects, title, deleted) VALUES (5, 8, 'Programing basics', false);
+INSERT INTO SUBJECT(id, ects, title, deleted) VALUES (6, 8, 'Economics', false);
+INSERT INTO SUBJECT(id, ects, title, deleted) VALUES (7, 8, 'Digital Electronics', false);
 
-INSERT INTO COURSE(id, start_date, end_date, subject_id, deleted) VALUES(1, '2021-01-01 00:00:00', '2021-10-01 00:00:00', 1, false);
-INSERT INTO COURSE(id, start_date, end_date, subject_id, deleted) VALUES(2, '2020-07-01 00:00:00', '2021-01-01 00:00:00', 2, false);
-INSERT INTO COURSE(id, start_date, end_date, subject_id, deleted) VALUES(3, '2020-07-01 00:00:00', '2021-01-01 00:00:00', 3, false);
-INSERT INTO COURSE(id, start_date, end_date, subject_id, deleted) VALUES(4, '2021-01-01 00:00:00', '2021-10-01 00:00:00', 4, false);
-INSERT INTO COURSE(id, start_date, end_date, subject_id, deleted) VALUES(5, '2021-01-01 00:00:00', '2021-10-01 00:00:00', 5, false);
+INSERT INTO COURSE(id, start_date, end_date, subject_id, deleted) VALUES(1, '2022-01-01 00:00:00', '2022-10-01 00:00:00', 1, false);
+INSERT INTO COURSE(id, start_date, end_date, subject_id, deleted) VALUES(2, '2021-01-01 00:00:00', '2021-06-01 00:00:00', 2, false);
+INSERT INTO COURSE(id, start_date, end_date, subject_id, deleted) VALUES(3, '2022-01-01 00:00:00', '2022-10-01 00:00:00', 3, false);
+INSERT INTO COURSE(id, start_date, end_date, subject_id, deleted) VALUES(4, '2022-01-01 00:00:00', '2022-10-01 00:00:00', 4, false);
+INSERT INTO COURSE(id, start_date, end_date, subject_id, deleted) VALUES(5, '2022-01-01 00:00:00', '2022-10-01 00:00:00', 5, false);
+INSERT INTO COURSE(id, start_date, end_date, subject_id, deleted) VALUES(6, '2022-01-01 00:00:00', '2022-10-01 00:00:00', 6, false);
+INSERT INTO COURSE(id, start_date, end_date, subject_id, deleted) VALUES(7, '2022-01-01 00:00:00', '2022-10-01 00:00:00', 7, false);
+INSERT INTO COURSE(id, start_date, end_date, subject_id, deleted) VALUES(8, '2022-01-01 00:00:00', '2022-10-01 00:00:00', 1, false);
 
 INSERT INTO TEACHER(id, course_id, employee_id, teaching_role_id, deleted) VALUES (1, 1, 1, 1, false);
 INSERT INTO TEACHER(id, course_id, employee_id, teaching_role_id, deleted) VALUES (2, 1, 2, 2, false);
@@ -31,23 +38,25 @@ INSERT INTO TEACHER(id, course_id, employee_id, teaching_role_id, deleted) VALUE
 INSERT INTO TEACHER(id, course_id, employee_id, teaching_role_id, deleted) VALUES (9, 5, 3, 1, false);
 INSERT INTO TEACHER(id, course_id, employee_id, teaching_role_id, deleted) VALUES (10, 5, 1, 3, false);
 
-INSERT INTO EXAM_PERIOD(id, end_time, name, start_time, deleted) VALUES(1, '2021-09-30 00:00:00', 'September', '2021-09-01 08:00:00', false);
+INSERT INTO EXAM_PERIOD(id, end_time, name, start_time, deleted) VALUES(1, '2021-10-20 00:00:00', 'October', '2021-09-29 00:00:00', false);
+INSERT INTO EXAM_PERIOD(id, end_time, name, start_time, deleted) VALUES(2, '2022-02-15 00:00:00', 'January', '2022-01-25 00:00:00', false);
+INSERT INTO EXAM_PERIOD(id, end_time, name, start_time, deleted) VALUES(3, '2022-03-15 00:00:00', 'February', '2022-02-15 00:00:00', false);
 
-INSERT INTO EXAM(id, exam_date_time, exam_enrolment_fee, course_id, exam_period_id, deleted) VALUES(1, '2021-06-29 00:00:00', 200.00, 1, 1, false);
-INSERT INTO EXAM(id, exam_date_time, exam_enrolment_fee, course_id, exam_period_id, deleted) VALUES(2, '2021-06-29 00:00:00', 200.00, 2, 1, false);
-INSERT INTO EXAM(id, exam_date_time, exam_enrolment_fee, course_id, exam_period_id, deleted) VALUES(3, '2021-06-29 00:00:00', 200.00, 3, 1, false);
-INSERT INTO EXAM(id, exam_date_time, exam_enrolment_fee, course_id, exam_period_id, deleted) VALUES(4, '2021-06-29 00:00:00', 200.00, 4, 1, false);
-INSERT INTO EXAM(id, exam_date_time, exam_enrolment_fee, course_id, exam_period_id, deleted) VALUES(5, '2021-06-29 00:00:00', 200.00, 5, 1, false);
+INSERT INTO EXAM(id, exam_date_time, exam_enrolment_fee, course_id, exam_period_id, deleted) VALUES(1, '2021-09-29 09:00:00', 200.00, 2, 1, false);
+INSERT INTO EXAM(id, exam_date_time, exam_enrolment_fee, course_id, exam_period_id, deleted) VALUES(2, '2022-01-25 10:00:00', 200.00, 2, 2, false);
+INSERT INTO EXAM(id, exam_date_time, exam_enrolment_fee, course_id, exam_period_id, deleted) VALUES(3, '2022-01-26 10:00:00', 200.00, 3, 2, false);
+INSERT INTO EXAM(id, exam_date_time, exam_enrolment_fee, course_id, exam_period_id, deleted) VALUES(4, '2022-01-27 10:00:00', 200.00, 4, 2, false);
+INSERT INTO EXAM(id, exam_date_time, exam_enrolment_fee, course_id, exam_period_id, deleted) VALUES(5, '2022-02-15 12:00:00', 200.00, 5, 3, false);
 
 INSERT INTO course_additional_points_type(id, name, deleted) VALUES(1, 'Project', false);
 INSERT INTO course_additional_points_type(id, name, deleted) VALUES(2, 'Colloquium', false);
 INSERT INTO course_additional_points_type(id, name, deleted) VALUES(3, 'Homework', false);
 
-INSERT INTO course_additional_points(id, name, pass_required, course_id, start_datetime, end_datetime, course_additional_points_type_id, deleted) VALUES(1, "First Colloquium", true, 1, '2021-06-27 00:00:00', '2021-06-27 00:02:00', 2, false);
-INSERT INTO course_additional_points(id, name, pass_required, course_id, start_datetime, end_datetime, course_additional_points_type_id, deleted) VALUES(2, "Second Colloquium", true, 1, '2021-06-27 00:00:00', '2021-06-27 00:02:00', 2, false);
-INSERT INTO course_additional_points(id, name, pass_required, course_id, course_additional_points_type_id, deleted) VALUES(3, "Math homework", false, 3, 3, false);
-INSERT INTO course_additional_points(id, name, pass_required, course_id, course_additional_points_type_id, deleted) VALUES(4, "Math homework", false, 4, 3, false);
-INSERT INTO course_additional_points(id, name, pass_required, course_id, course_additional_points_type_id, deleted) VALUES(5, "Final project", true, 1, 1, false);
+INSERT INTO course_additional_points(id, name, pass_required, course_id, start_datetime, end_datetime, course_additional_points_type_id, deleted) VALUES(1, "First Colloquium", true, 1, '2022-09-29 10:00:00', '2022-09-29 12:00:00', 2, false);
+INSERT INTO course_additional_points(id, name, pass_required, course_id, start_datetime, end_datetime, course_additional_points_type_id, deleted) VALUES(2, "Second Colloquium", true, 1, '2022-10-30 10:00:00', '2022-10-30 12:00:00', 2, false);
+INSERT INTO course_additional_points(id, name, pass_required, course_id, start_datetime, end_datetime, course_additional_points_type_id, deleted) VALUES(3, "Math homework", false, 3, '2022-01-01 00:00:00', '2022-10-01 00:00:00', 3, false);
+INSERT INTO course_additional_points(id, name, pass_required, course_id, start_datetime, end_datetime, course_additional_points_type_id, deleted) VALUES(4, "Math homework", false, 4, '2022-01-01 00:00:00', '2022-10-01 00:00:00', 3, false);
+INSERT INTO course_additional_points(id, name, pass_required, course_id, start_datetime, end_datetime, course_additional_points_type_id, deleted) VALUES(5, "Final project", true, 1, '2022-01-01 00:00:00', '2022-10-01 00:00:00', 1, false);
 
 
 INSERT INTO STUDENT(id, email, first_name, last_name, password, username, deleted) VALUES(1, 'mail1@gmail.com', 'John1', 'Doe1', '$2y$12$axnGvlxM6nxl7hb/Saxhy.wZWqg6lgVafGknsXA7jvG7qhZS9zyGq', 'johndoe1', false); --password123
@@ -84,7 +93,7 @@ INSERT INTO COURSE_ENROLMENT(id, course_id, student_id, deleted) VALUES(15, 4, 5
 INSERT INTO COURSE_ENROLMENT(id, course_id, student_id, deleted) VALUES(16, 4, 4, false);
 INSERT INTO COURSE_ENROLMENT(id, course_id, student_id, deleted) VALUES(17, 4, 5, false);
 
-INSERT INTO EXPENSE(id, timestamp, amount, account_id, deleted) VALUES(1, '2021-06-29 00:00:00', 15.00, 1, false);
+INSERT INTO EXPENSE(id, timestamp, amount, account_id, deleted) VALUES(1, '2022-06-29 00:00:00', 15.00, 1, false);
 
 INSERT INTO EXAM_ENROLMENT(id, exam_id, course_enrolment_id, expense_id, deleted) VALUES(1, 1, 1, 1, false);
 
